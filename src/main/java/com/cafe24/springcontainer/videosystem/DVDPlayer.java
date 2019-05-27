4ea068fb9f841de6767421a8bac9fc129c4c233d
Bking625 <ddiddikk@naver.com>
2019-05-27 15:00:13 +0900

    +explicit configure

---
+++ src/main/java/com/cafe24/springcontainer/videosystem/DVDPlayer.java
@@ -0,0 +1,26 @@
+package com.cafe24.springcontainer.videosystem;
+
+public class DVDPlayer {
+	private DigitalVideoDisc dvd;
+	
+	public DVDPlayer() {		
+	}	
+	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public void setDigitalVideoDisc(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public void play() {
+		dvd.play();
+	}
+
+
+}
